54ff1f6a6a1712ca48f3b670e4e25d1ded449632
Cesartwothousands <cesarchen616@outlook.com>
2023-01-05 23:59:04 -0500

    Card_0

---
+++ Card.java
@@ -0,0 +1,33 @@
+package poker;
+
+public class Card {
+    private short rank, suit;
+
+    private static String[] suits={"Heart","Spade","Diamond","Club"};
+    private static String[] ranks={"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
+    //Define 4 suits and 13 ranks
+
+    Card(short suit, short rank)
+    {
+        this.suit=suit;
+        this.rank=rank;
+    }
+    //Constract function
+
+    public short getSuit() {
+        return suit;
+    }
+    public short getRank() {
+        return rank;
+    }
+
+    public static String rankAsString( int __rank ) {
+        return ranks[__rank];
+    }
+
+    public @Override String toString()
+    {
+        return ranks[rank] + " of " + suits[suit];
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Texas-Holdem-Evaluator
+
+Make a program that can create, 
+evaluate and compare 5-card poker hands.
+
+We have **cards**, **decks**, and **hands**.
+
+- Card will be a class that contains rank and suit variables,
+
+- Deck will be a container for Cards, and 
+
+- Hand will be where we evaluate and compare the poker hands.
+
